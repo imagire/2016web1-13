144177576bf4310a46880b02a1c36eed478faf82
Takashi Imagire <imagire@gmail.com>
2016-06-23 22:15:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,8 +2,8 @@ var separate_time = function(date){
   var time = date.getTime();
   var sec   = Math.floor((time / 1000) % 60);
   var min   = Math.floor((time / 1000 / 60) % 60);
-  var hours = Math.floor((time / 1000 / 60 / 60) % 60);
-  var days  = Math.floor((time / 1000 / 60 / 60 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor( time / 1000 / 60 / 60 / 24);
   return [sec, min, hours, days];
 }
 
