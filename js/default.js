8711f5b2b675b1fe65b36c79e58410b2d4df07c9
Takashi Imagire <imagire@gmail.com>
2016-06-23 22:50:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,10 +8,10 @@ var separate_time = function(time){
 
 var now = new Date();
 var target = new Date(2020,7,24,0,0,0,0);
-//var diff = target.getTime() - now.getTime() ;
-var diff = now.getTime() ;
+var diff = target.getTime() - now.getTime() ;
 var counter = separate_time(diff);
 document.getElementById('countdown').textContent = 
+  '東京オリンピックまであと' +
   counter[3] + '日' + 
   counter[2] + '時' + 
   counter[1] + '分' + 
