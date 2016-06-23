495ec52350e39470eaca93ad7b591465ddf02e68
Takashi Imagire <imagire@gmail.com>
2016-06-23 22:14:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,12 @@
+var separate_time = function(date){
+  var time = date.getTime();
+  var sec   = Math.floor((time / 1000) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 60);
+  var days  = Math.floor((time / 1000 / 60 / 60 / 60) % 60);
+  return [sec, min, hours, days];
+}
 
+var now = new Date();
+counter = separate_time(now);
+document.getElementById('countdown').textContent = counter[2] + '時間' + counter[1] + '分' + counter[0] + '秒';
