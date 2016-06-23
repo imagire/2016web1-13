db632a06fdab401bad39390168703a4591541520
Takashi Imagire <imagire@gmail.com>
2016-06-23 23:01:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,13 +6,21 @@ var separate_time = function(time){
   return [sec, min, hours, days];
 }
 
-var now = new Date();
-var target = new Date(2020,7,24,0,0,0,0);
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent = 
-  '東京オリンピックまであと ' +
-  counter[3] + '日' + 
-  counter[2] + '時間' + 
-  counter[1] + '分' + 
-  counter[0] + '秒';
+var update = function(){
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_time(diff);
+  document.getElementById('countdown').textContent = 
+    '東京オリンピックまであと ' +
+    counter[3] + '日' + 
+    counter[2] + '時間' + 
+    counter[1] + '分' + 
+    counter[0] + '秒';
+  reflesh();// タイマーを起動
+}
+
+var reflesh() = function(){
+  setTimeout(update, 1000);//1000ミリ秒待ってからupdateを実行
+}
+update();// 最初の更新
