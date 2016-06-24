de97e4d2c54890853b763f3fc3bce4dbcb6b60f4
Takashi Imagire <imagire@gmail.com>
2016-06-24 13:00:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -39,7 +39,7 @@ var last_date = getCookie('lastDate');
 if(last_date){
     document.getElementById('cookie').textContent = '前回訪れた時間：' + last_date;
 }else{
-    document.getElementById('cookie').textContent = "はじめまして";
+    document.getElementById('cookie').textContent = 'はじめまして';
 }
 var current_time = new Date();
 getCookie('lastDate', current_time.toString(), 7);
